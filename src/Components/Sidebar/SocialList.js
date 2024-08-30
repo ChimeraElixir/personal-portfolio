f657c5f15e3fbef79155d8803378b13ee73b3b32
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-08-31 00:22:36 +0530

    updated final esland error

--- src/Components/Sidebar/SocialList.js
+++ src/Components/Sidebar/SocialList.js
@@ -1,33 +1,22 @@
-import React from "react";
+import React from "react"
 import IonIcon from "@reacticons/ionicons"
 
-import { socialLists } from "../../data";
-const logos = [
-  {
-    name: "logo-facebook",
-  },
-  {
-    name: "logo-twitter",
-  },
-  {
-    name:"logo-instagram"
-  },
-];
+import { socialLists } from "../../data"
 
 const SocialList = () => {
   return (
-    <ul className='social-list'>
-      {socialLists.map((socialList,index) => {
+    <ul className="social-list">
+      {socialLists.map((socialList, index) => {
         return (
-          <li key={index} className='social-item'>
-            <a href={socialList.link} className='social-link'>
+          <li key={index} className="social-item">
+            <a href={socialList.link} className="social-link">
               <IonIcon name={socialList.logo}></IonIcon>
             </a>
           </li>
-        );
+        )
       })}
     </ul>
-  );
-};
+  )
+}
 
-export default SocialList;
+export default SocialList
